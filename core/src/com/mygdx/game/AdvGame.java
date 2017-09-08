1c0b41a3631bedb873c5a57e691bfa57f0b31f40
Andrew Hatch <ahatch@amplience.com>
2017-09-08 18:37:51 +0100

    use of viewport and stage

--- core/src/com/mygdx/game/AdvGame.java
+++ core/src/com/mygdx/game/AdvGame.java
@@ -3,31 +3,39 @@ package com.mygdx.game;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
+import com.badlogic.gdx.utils.Scaling;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.badlogic.gdx.utils.viewport.ScalingViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
 
 public class AdvGame extends ApplicationAdapter {
-  private SpriteBatch batch;
-  private Texture img;
+  private Stage stage;
+  private Viewport viewport;
 
   @Override
   public void create() {
-    batch = new SpriteBatch();
-    img = new Texture("badlogic.jpg");
+    float width = Gdx.graphics.getWidth();
+    float height = Gdx.graphics.getHeight();
+    this.viewport = new FitViewport(width, height);
+    stage = new Stage(viewport);
+    stage.addActor(new Image(new Texture("ef2000.png")));
   }
 
   @Override
   public void render() {
-    Gdx.gl.glClearColor(1, 0, 0, 1);
+    Gdx.gl.glClearColor(0.2f, 0.2f, 0.2f, 1.0f);
     Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-    batch.begin();
-    batch.draw(img, 0, 0);
-    batch.end();
+    stage.draw();
   }
 
   @Override
-  public void dispose() {
-    batch.dispose();
-    img.dispose();
+  public void resize (int width, int height) {
+    stage.getViewport().update(width, height, true);
   }
 }
